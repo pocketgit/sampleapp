56d76f5ea91a46463764b97eabe48ed9634cc030
pocketgit <anuj.agrawal@adways-india.com>
2017-11-22 15:09:37 +0530

    first commit

---
+++ app/src/main/java/sampleapp/pmsdk/com/sampleapppmsdk/SampleActivity.java
@@ -0,0 +1,31 @@
+package sampleapp.pmsdk.com.sampleapppmsdk;
+
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.pokkt.sdk.pocketmoney.activity.PMSdk;
+
+
+public class SampleActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample);
+
+        Button showOfferwall = findViewById(R.id.showOfferwalls);
+        showOfferwall.setOnClickListener(this);
+
+        PMSdk.initializePMSdk("78374919acfa960412febddb66b3c8ec", ContextCompat.getColor(this, R.color.colorPrimary),ContextCompat.getColor(this, android.R.color.white),"App Zone");
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        PMSdk.showPMSdkOfferWall(this);
+    }
+}
